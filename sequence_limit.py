67f7a7df15b316ba9716d71603128ce254b82959
epsilonia <129690268+epsilonia@users.noreply.github.com>
2023-04-02 16:19:48 -0400

    Update sequence_limit.py

--- sequence_limit.py
+++ sequence_limit.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 import plotly.graph_objects as go
-import pandas as pd
 import numpy as np
 
 def get_figure(eps, N):
@@ -155,6 +154,10 @@ def get_figure(eps, N):
 
 
 st.markdown("<h1 style='text-align: center; color: black;'>Finite limit of a real sequence</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: left; color: rgba(94,192,231,255);'>Instructions</h3>", unsafe_allow_html=True)
+st.markdown("(1) Use the slider to change the value of ε", unsafe_allow_html=True)
+st.markdown("(2) For each chosen value of ε, adjust N to obtain a good value", unsafe_allow_html=True)
+st.markdown("A good value of N is one such that all terms with indices greater than N are inside the strip of center 2 and radius ε", unsafe_allow_html=True)
 eps = st.slider("epsilon", min_value=0.01, max_value=1., value=.5, step=.01, disabled=False, label_visibility="visible")
 N = st.slider("N", min_value=1, max_value=30, value=5, step=1, disabled=False, label_visibility="visible")
 fig = get_figure(eps,N)
