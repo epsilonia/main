b2a33fe2c323cf966dd97b6e84422de834576a47
epsilonia <129690268+epsilonia@users.noreply.github.com>
2023-04-10 23:49:25 -0400

    Hiding footnote

--- sequence_limit.py
+++ sequence_limit.py
@@ -136,7 +136,7 @@ def get_figure(eps, N):
             go.Scatter(
                 visible=True,
                 mode='markers',
-                marker= {'color':'rgba(255,0,0,255)','size':10},
+#                 marker= {'color':'rgba(255,0,0,255)','size':10},
                 x=xred,
                 y=2*(1-np.exp(-xred/5)),
                 hoverinfo = 'skip',
@@ -146,12 +146,18 @@ def get_figure(eps, N):
         )
         
         title = "This is NOT a good value for N. Points in red are outside the strip."
-        title_color = 'rgba(255,0,0,255)'
+#         title_color = 'rgba(255,0,0,255)'
 
     fig.update_layout(title = title,   title_font_color=title_color)
     
     return fig
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
 
 st.markdown("<h1 style='text-align: center;'>Finite limit of a real sequence</h1>", unsafe_allow_html=True)
 st.markdown("""---""")
