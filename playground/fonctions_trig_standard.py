3292fa994b1931d2acaab34950e582669abc059a
epsilonia <129690268+epsilonia@users.noreply.github.com>
2023-09-27 18:06:43 -0400

    Update code that write into appstat.csv

--- playground/fonctions_trig_standard.py
+++ playground/fonctions_trig_standard.py
@@ -79,14 +79,14 @@ def get_figure(M,A,P,T):
 
 
 try:
-    stats = pd.read_csv('appstat.csv')
+    stats = pd.read_csv('./appstat.csv')
 except:
     stats = pd.DataFrame(columns = ['time'])
 
 now = datetime.datetime.now()
 st.write(now)
 stats.loc[len(stats),'time']=now
-stats.to_csv('appstat.csv',index=False)
+stats.to_csv('./appstat.csv',index=False)
 
 
 st.markdown("<h1 style='text-align: center;'>Forme standard d'une fonction trigonométrique</h1>", unsafe_allow_html=True)
